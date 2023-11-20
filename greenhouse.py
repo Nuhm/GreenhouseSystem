d07f5a6d7edf42be62b790479071d5e1e340569e
J-shw <91017208+J-shw@users.noreply.github.com>
2023-11-20 10:32:26 +0000

    Removed all global values + added new class system
    - Global variables are now stored as a class
    - Small bug fixes
    - Other small changes

--- greenhouse.py
+++ greenhouse.py
@@ -1,80 +1,78 @@
 import time, datetime, os, psutil, Adafruit_DHT, csv, json, webhook
 import RPi.GPIO as io
-global crash
 
-# File/File paths
-logFilePath = "/home/green/static/data/logs/"
-crashFilePath = "/home/green/static/data/crashLogs/"
-logFilesRow = ["indoorHumid","indoorTemp","outsideHumid","outsideTemp","boxHumid","boxTemp","cpuTemp","cpuFreq","storageUsed","time"]
-configPath = "/home/green/static/data/config.json"
-# - - - - - - - 
+class gh:
+    # File/File paths
+    logFilePath = "/home/green/static/data/logs/"
+    crashFilePath = "/home/green/static/data/crashLogs/"
+    logFilesRow = ["indoorHumid","indoorTemp","outsideHumid","outsideTemp","boxHumid","boxTemp","cpuTemp","cpuFreq","storageUsed","time"]
+    configPath = "/home/green/static/data/config.json"
+    # - - - - - - - 
 
-# Config data
-configFile = open(configPath)
-configData = json.load(configFile)
-# - - - - - - - 
+    # Config data
+    configFile = open(configPath)
+    configData = json.load(configFile)
+    # - - - - - - - 
 
-# Variables setup
-fanSpeed = 100
-deviceData = []
-data = []
-autoWatering = False
-watering = False
-window = False # False is closed | True is open
-update = False # Used for webhook
-time_end = time.time()
-crash = [False, None, None]
-alerted = False
-crashAlerted = False
-lastCrashTime = 0
-running = True
-# - - - - - - - 
+    # Variables setup
+    allData = []
+    deviceData = []
+    data = []
+    autoWatering = False
+    watering = False
+    window = False # False is closed | True is open
+    update = False # Used for webhook
+    time_end = time.time()
+    crash = [False, None, None]
+    alerted = False
+    crashAlerted = False
+    lastCrashTime = 0
+    # - - - - - - - 
 
-# Pin setup
-boxSensor = 2
-insideSensor = 3
-outsideSensor = 4
-waterRelay = 27
-servo_pin = 18
-# - - - - - - - 
+    # Last check values
+    lastOutsideHumidity = 0
+    lastOutsideTemperature = 0
+    lastInsideHumidity = 0
+    lastInsideTemperature = 0
+    # - - - - - - -
 
-# Sensors setup
-io.setmode(io.BCM)
-io.setup(waterRelay, io.OUT)
-io.setup(servo_pin, io.OUT)
-sensor = Adafruit_DHT.DHT22
-# - - - - - - - 
+    # Pin setup
+    boxSensor = 2
+    insideSensor = 3
+    outsideSensor = 4
+    waterRelay = 27
+    servo_pin = 18
+    # - - - - - - - 
 
-# Create a PWM instance
-servo_pwm = io.PWM(servo_pin, 50)  # 50 Hz PWM frequency
-servo_pwm.start(0) # Starting PWM signal with duty cycle of 0 (corresponds to 0 degrees)
-# - - - - - - - 
+    # Sensors setup
+    io.setmode(io.BCM)
+    io.setup(waterRelay, io.OUT)
+    io.setup(servo_pin, io.OUT)
+    sensor = Adafruit_DHT.DHT22
+    # - - - - - - - 
+
+    # Create a PWM instance
+    servo_pwm = io.PWM(servo_pin, 50)  # 50 Hz PWM frequency
+    servo_pwm.start(0) # Starting PWM signal with duty cycle of 0 (corresponds to 0 degrees)
+    # - - - - - - - 
 
 for angle in range(0, 90): #Close window on boot
     duty_cycle = angle / 18.0 + 2.5
-    servo_pwm.ChangeDutyCycle(duty_cycle)
+    gh.servo_pwm.ChangeDutyCycle(duty_cycle)
     time.sleep(0.05)  # Wait for 50ms before changing angle again
 
 
 def water(state): # Used to turn on/off the water system
-    global watering
-    print(state)
     if state:
         #Turn water on
-        watering = True
-        io.output(waterRelay, io.HIGH)
+        gh.watering = True
+        io.output(gh.waterRelay, io.HIGH)
     else:
         #Turn water off
-        watering = False
-        io.output(waterRelay, io.LOW)
+        gh.watering = False
+        io.output(gh.waterRelay, io.LOW)
 
 def getDeviceData(): # Used to get device data
-    global deviceData
-    global pwm
-    global fanSpeed
-
-    fanSpeed = 0
-
     freq = psutil.cpu_freq()
     disk = psutil.disk_usage('/')
 
@@ -84,24 +82,16 @@ def getDeviceData(): # Used to get device data
 
     usedDisk = round(disk.used / (1024*1024*1024), 2) #GB    
     
-    deviceData = [cpuTemp,cpuFreq,usedDisk]
+    return [cpuTemp,cpuFreq,usedDisk]
 
-def getData(): # Used to get tempuratues from sensors
-    global data
-    global lastOutsideHumidity
-    global lastOutsideTemperature
-    global lastInsideHumidity
-    global insideTemperature
-    global update
-    global window
-
+def getData(configData): # Used to get tempuratues from sensors
     tempHigh = configData['tempSettings']['high']
     tempLow = configData['tempSettings']['low']
 
     windowTemp = configData['heatControl']['high']
-    insideHumidity, insideTemperature = Adafruit_DHT.read_retry(sensor, insideSensor)
-    outsideHumidity, outsideTemperature = Adafruit_DHT.read_retry(sensor, outsideSensor)
-    boxHumidty, boxTemperature = Adafruit_DHT.read_retry(sensor, boxSensor)
+    insideHumidity, insideTemperature = Adafruit_DHT.read_retry(gh.sensor, gh.insideSensor)
+    outsideHumidity, outsideTemperature = Adafruit_DHT.read_retry(gh.sensor, gh.outsideSensor)
+    boxHumidty, boxTemperature = Adafruit_DHT.read_retry(gh.sensor, gh.boxSensor)
 
     if insideHumidity is None and insideTemperature is None:
         insideHumidity = 0
@@ -115,63 +105,61 @@ def getData(): # Used to get tempuratues from sensors
 
     if int(outsideHumidity) > 100:
         try:
-            outsideHumidity = lastOutsideHumidity
-            outsideTemperature = lastOutsideTemperature
+            outsideHumidity = gh.lastOutsideHumidity
+            outsideTemperature = gh.lastOutsideTemperature
         except:
             None
     else:
-        lastOutsideHumidity = outsideHumidity
-        lastOutsideTemperature = outsideTemperature
+        gh.lastOutsideHumidity = outsideHumidity
+        gh.lastOutsideTemperature = outsideTemperature
     
     if int(insideHumidity) > 100:
         try:
-            insideHumidity = lastInsideHumidity
-            insideTemperature = lastInsideTemperature
+            insideHumidity = gh.lastInsideHumidity
+            insideTemperature = gh.lastInsideTemperature
         except:
             None
     else:
-        lastInsideHumidity = insideHumidity
-        lastInsideTemperature = insideTemperature
+        gh.lastInsideHumidity = insideHumidity
+        gh.lastInsideTemperature = insideTemperature
     
     server = configData['webhook']['server']
 
-    if update:
+    if gh.update:
         if outsideTemperature <= tempLow:
             key = configData['webhook']['keys']['under']
             response = webhook.send(server, key)
             if response == 200:
-                update = False
+                gh.update = False
 
     else:
         if outsideTemperature >=tempHigh:
             key = configData['webhook']['keys']['over']
             response = webhook.send(server, key)
             if response == 200:
-                update = True
+                gh.update = True
     
-    if insideTemperature >= windowTemp and window == False: #Open window
+    if insideTemperature >= windowTemp and gh.window == False: #Open window
         for angle in range(90, -1, -1):
             duty_cycle = angle / 18.0 + 2.5
-            servo_pwm.ChangeDutyCycle(duty_cycle)
+            gh.servo_pwm.ChangeDutyCycle(duty_cycle)
         time.sleep(2)
-        window = True
+        gh.window = True
         
-    if insideTemperature <= (windowTemp - 1) and window == True: #Close window
+    if insideTemperature <= (windowTemp - 1) and gh.window == True: #Close window
         time.sleep(0.2)
         for angle in range(0, 90):
             duty_cycle = angle / 18.0 + 2.5
-            servo_pwm.ChangeDutyCycle(duty_cycle)
+            gh.servo_pwm.ChangeDutyCycle(duty_cycle)
             time.sleep(0.05)  # Wait for 50ms before changing angle again
-        servo_pwm.ChangeDutyCycle(0)
+        gh.servo_pwm.ChangeDutyCycle(0)
         time.sleep(5)
-        window = False
+        gh.window = False
     
 
-    data = [round(insideHumidity,1), round(insideTemperature,1), round(outsideHumidity,1), round(outsideTemperature,1), round(boxHumidty,1), round(boxTemperature,1)]
+    return [round(insideHumidity,1), round(insideTemperature,1), round(outsideHumidity,1), round(outsideTemperature,1), round(boxHumidty,1), round(boxTemperature,1)]
 
-def log(data): # Used to save/log data
-    global crash
-
+def log(data : list, current_time : float): # Used to save/log data
     dataToSave = []
 
     for x in data:
@@ -182,25 +170,21 @@ def log(data): # Used to save/log data
     formatted_time = current_time.strftime("%H:%M:%S")
     dataToSave.append(formatted_time)
 
-    filename = logFilePath+formatted_date+".csv"
+    filename = gh.logFilePath+formatted_date+".csv"
 
-    try:
-        if not os.path.exists(filename):
-            row = logFilesRow
-            with open(filename, 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(row)
 
-        # data.append(formatted_time)
-        with open(filename, 'a', newline='') as file:
+    if not os.path.exists(filename):
+        row = gh.logFilesRow
+        with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(dataToSave)
-    except Exception as e:
-        crash = [True, "logging | " +str(e), time.time()]
+            writer.writerow(row)
 
-def checkSchedule(): # Used to update watering schedule
-    global autoWatering
-    global time_end
+    # data.append(formatted_time)
+    with open(filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(dataToSave)
+
+def checkSchedule(configData, current_time : float): # Used to update watering schedule
 
     day_of_week = current_time.strftime("%A")
     time = current_time.strftime("%H:%M")
@@ -208,57 +192,50 @@ def checkSchedule(): # Used to update watering schedule
     schedule = configData['schedule']
     duration = configData['watering']['duration']
     if schedule[day_of_week] != "false":
-        if str(time) == schedule[day_of_week] and autoWatering == False:
-            time_end = time.time() + duration
-            autoWatering = True
+        if str(time) == schedule[day_of_week] and gh.autoWatering == False:
+            gh.time_end = time.time() + duration
+            gh.autoWatering = True
             water(True)
-        elif autoWatering:
-            if time.time() >= time_end:
+        elif gh.autoWatering:
+            if time.time() >= gh.time_end:
                 water(False)
-                autoWatering = False
+                gh.autoWatering = False
 
 def systemState(): # return layout - [Status, Running, Crashed, Error]
-    global crash
 
-    if crash[0] != True:
+    if gh.crash[0] != True:
         try:
-            global running
-            return ([200, running, False])
+            return ([200, True, False])
         except Exception as e:
             return([500, None, True, str(e)])
     else:
-        return ([200, False, crash[0], crash[1]])
+        return ([200, False, gh.crash[0], gh.crash[1]])
 
 def currentData():
-    global allData
-    global watering
-    global autoWatering
-    global fanSpeed
-    global window
+    dataToShare = [gh.watering, gh.autoWatering]
 
-    dataToShare = [watering, autoWatering]
-
-    for x in allData:
+    for x in gh.allData:
         for i in x:
             dataToShare.append(i)
-    dataToShare.append(fanSpeed)
-    dataToShare.append(window)
+    dataToShare.append(0) # Used to be fan speed | Remove soon
+    dataToShare.append(gh.window)
     dataToShare.append(datetime.datetime.now().strftime("%H:%M"))
     return dataToShare
 
 def getSchedule():
-    data = [configData['schedule'], configData['watering']]
-    return data
+    with open(gh.configPath) as config_file:
+        configData = json.load(config_file)
+
+    return [configData['schedule'], configData['watering']]
 
 
 def updateJson(data):
-    global configPath
 
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     try:
         # Load the existing JSON data from the file
-        with open(configPath, "r") as infile:
+        with open(gh.configPath, "r") as infile:
             config = json.load(infile)
         i = 0
         while i < 7:
@@ -268,23 +245,20 @@ def updateJson(data):
         config['watering']['duration'] = int(data[1])
 
         # Write the modified object back to the JSON file
-        with open(configPath, "w") as outfile:
+        with open(gh.configPath, "w") as outfile:
             json.dump(config, outfile, indent=4)
     except Exception as e:
         return [500, str(e)]
     
     return [200, "None"]
 
-def logCrash(crashData : list): #Changed this to save actual time not time in long format
-    global crashAlerted
-    global crashFilePath
-    global lastCrashTime
+def logCrash(configData, crashData : list): #Changed this to save actual time not time in long format
 
     # [True, "getData() | local variable 'innerL' referenced before assignment", 1689893586.5361037] | 2023-07-20
 
     crash_time = crashData[2]
-    if crash_time != lastCrashTime:
-        lastCrashTime = crash_time
+    if crash_time != gh.lastCrashTime:
+        gh.lastCrashTime = crash_time
 
         time = datetime.datetime.now()
 
@@ -293,7 +267,7 @@ def logCrash(crashData : list): #Changed this to save actual time not time in lo
         time_obj = datetime.datetime.fromtimestamp(crash_time)
         crash_time = time_obj.strftime("%H:%M:%S")
 
-        filename = f"{crashFilePath}{formatted_date}.txt"
+        filename = f"{gh.crashFilePath}{formatted_date}.txt"
         row = f"{str(crashData[1])}, {str(crash_time)}\n"
 
         if not os.path.exists(filename):
@@ -303,59 +277,60 @@ def logCrash(crashData : list): #Changed this to save actual time not time in lo
             with open(filename, 'a', newline='') as file:
                 file.write(row)
         
-    if crashAlerted == False:
+    if gh.crashAlerted == False:
         server = configData['webhook']['server']
         key = configData['webhook']['keys']['crash']
         response = webhook.send(server, key)
         if response == 200:
-            crashAlerted = True
+            gh.crashAlerted = True
 
 
 
 def start(): 
-    global configFile
-    global configData
-    global current_time
-    global crash
-    global allData
     runTime = time.time()
 
-    while running:
+    while True:
 
         if time.time() >= runTime:
-            runTime = time.time() + configData['updateFreq']['time']
+
+            try:
+                with open(gh.configPath) as config_file:
+                    configData = json.load(config_file)
+
+                runTime = time.time() + configData['updateFreq']['time']
+            except Exception as e:
+                gh.crash = [True, "config | " +str(e), time.time()]
             
-            if crash[0]:
-                logCrash(crash)
+            if gh.crash[0]:
+                logCrash(configData, gh.crash)
 
             try: 
-                if time.time() >= crash[2] + 30 :crash = [False, None, None]
+                if time.time() >= gh.crash[2] + 30: gh.crash = [False, None, None]
             except: None
-            try:
-                configFile = open(configPath)
-                configData = json.load(configFile)
-            except Exception as e:
-                crash = [True, "config | " +str(e), time.time()]
 
-            # Trigger the water system
             current_time = datetime.datetime.now()
+
+            # Collect data
             try:
-                getDeviceData()
+                gh.deviceData = getDeviceData()
             except Exception as e:
-                crash = [True, "getDeviceData() | " +str(e), time.time()]
+                gh.crash = [True, "getDeviceData() | " +str(e), time.time()]
             
             try:
-                getData()
+                gh.data = getData(configData)
             except Exception as e:
-                crash = [True, "getData() | " + str(e), time.time()]
+                gh.crash = [True, "getData() | " + str(e), time.time()]
+            
+            gh.allData = [gh.data,gh.deviceData]
             
             try:
-                checkSchedule()
+                checkSchedule(configData, current_time)
             except Exception as e:
-                crash = [True, "checkSchedule() | " +str(e), time.time()]
+                gh.crash = [True, "checkSchedule() | " +str(e), time.time()]
 
-            allData = [data,deviceData]
-
-            log(allData)
+            try:
+                log(gh.allData, current_time)
+            except Exception as e:
+                gh.crash = [True, "log() | " +str(e), time.time()]
 
         time.sleep(0.2)
